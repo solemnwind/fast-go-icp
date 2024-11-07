7110646d67285549a9e2de67c7df7186271fe097
Mufeng Xu <mufeng.xu@outlook.com>
2024-11-07 17:38:32 -0500

    initialize codebase

---
+++ src/fgoicp.cpp
@@ -0,0 +1,14 @@
+#include "fgoicp.hpp"
+#include <iostream>
+
+int foo()
+{
+    std::cout << "Fast Go-ICP" << std::endl;
+    return 1;
+}
+
+int main(int argc, char* argv[])
+{
+    foo();
+    return 0;
+}
---
+++ src/fgoicp.hpp
@@ -0,0 +1,7 @@
+#ifndef FGOICP_H
+#define FGOICP_H
+#include <tinyply.h>
+
+int foo();
+
+#endif // FGOICP_H
